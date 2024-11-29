10c2cf77baee9b1a99052ee8ba37909dfc024e46
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-11-29 07:44:21 +0530

    Update apiService.js

--- ChecklistSystem/services/apiService.js
+++ ChecklistSystem/services/apiService.js
@@ -1,1 +1,16 @@
+const axios = require("axios");
 
+const fetchApplicationData = async () => {
+  const API_URL =
+    "http://qa-gb.api.dynamatix.com:3100/api/applications/getApplicationById/67339ae56d5231c1a2c63639";
+
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    throw new Error("Failed to fetch application data");
+  }
+};
+
+module.exports = { fetchApplicationData };
