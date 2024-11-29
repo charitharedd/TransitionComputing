fee3ce42c25743d26883b25a540dd67e06bea2be
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-11-29 07:46:11 +0530

    Update app.js

--- ChecklistSystem/app.js
+++ ChecklistSystem/app.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const path = require("path");
+const { evaluateChecklist } = require("./controllers/checklistController");
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get("/api/checklist", async (req, res) => {
+  try {
+    const results = await evaluateChecklist();
+    res.json(results);
+  } catch (error) {
+    res.status(500).send("Error processing checklist");
+  }
+});
+
+app.listen(PORT, () => console.log(Server running on http://localhost:${PORT}));
